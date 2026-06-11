d71896ff09b89b56134c75c3bf84e1afa63acb5f
d9178604-create <d9178604@gmail.com>
2026-06-11 13:02:57 +0000

    master service changes

--- server/master/masterService.js
+++ server/master/masterService.js
@@ -12,24 +12,29 @@ const getMenu = async (company_id = 1) => {
       accountingItems.push("Cost Centre");
     }
     menu.push({ title: "Accounting Masters", items: accountingItems });
+     
+    if(features.enable_payment_request_qr) {
+      const paymentRequest = ["Merchant Profile"];
+      menu.push({ title: "Payment Request",items:paymentRequest});
+    }
 
     if (features.maintain_inventory !== 0) {
-      const inventoryItems = ["Stock Group", "Stock Category", "Stock Items", "Unit", "Location"];
+      const inventoryItems = ["Stock Group", "Stock Category", "Stock Items", "Unit", "Location", "Price levels", "Price list (Stock Group)", "Price list (Stock Category)"];
       menu.push({ title: "Inventory Masters", items: inventoryItems });
     }
     menu.push({
       title: "Statutory Masters",
-      items: ["GST Registration", "GST Classification", "TCS Nature of Goods", "TDS Nature of Payment"]
+      items: ["GST Registration", "GST Classification", "TCS Nature of Goods", "TDS Nature of Payment","Tax Units"]
     });
 
     menu.push({
       title: "Statutory Details",
-      items: ["Company GST Details", "PAN / CIN Details"]
+      items: ["Company GST Details","TDS Nature of payments","TCS Nature of goods","VAT Registration Details","Excise Registration Details", "PAN / CIN Details"]
     });
 
     menu.push({
       title: "Payroll Masters",
-      items: ["Employee Category", "Employee Group", "Employee", "Attendance Type", "Pay Head", "Payroll Unit", "Salary Structure"]
+      items: ["Employee Category", "Employee","Units(work)", "Employee Group", "Attendance / Production type", "Pay Heads","Payroll Voucher Type", "Salary Structure"]
     });
 
     return { success: true, menu };
